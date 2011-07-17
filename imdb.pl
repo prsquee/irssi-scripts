8ce9463ae28359727773c437bcd91097d1e4712d
sQuEE <sq@fnord.com.ar>
2011-07-17 17:42:09 -0300

    fixxx

--- imdb.pl
+++ imdb.pl
@@ -1,12 +1,31 @@
 #imdb api
 #http://imdbapi.com/
 
+#$imdb struct
+#'ID' => 'tt0290978',
+#'Director' => 'N/A',
+#'Poster' => 'http://ia.media-imdb.com/images/M/MV5BMTQ2MzAxODI3M15BMl5BanBnXkFtZTcwNTE2MjAwMQ@@._V1._SX320.jpg',
+#'Actors' => 'Ricky Gervais, Martin Freeman, Mackenzie Crook, Lucy Davis',
+#'Runtime' => '29 mins',
+#'Genre' => 'Comedy, Drama',
+#'Rating' => '9.2',
+#'Released' => '23 Jan 2003',
+#'Response' => 'True',
+#'Votes' => '16626',
+#'Title' => 'The Office',
+#'Year' => '2001',
+#'Rated' => 'TV-MA',
+#'Writer' => 'N/A',
+#'Plot' => 'The story of an office that faces closure when the company decides to downsize
+
 use Irssi qw(command_bind signal_add print active_win server_find_tag ) ;
 use strict;
 use LWP::UserAgent;
 use URI::Escape qw( uri_escape );
-use Data::Dumper;
+#use Data::Dumper;
 use JSON;
+
+
 sub msg_pub {
 	my($server, $text, $nick, $mask,$chan) = @_;
 	do_imdb($text, $chan, $server) if ($server->{tag} =~ /3dg|fnode|lia|gsg/ and $text =~ /^!imdb/);
@@ -39,8 +58,6 @@ sub do_imdb {
 		sayit($server,$chan,$title);
 		sayit($server,$chan,$genre);
 		sayit($server,$chan,$plot);
-		my $a = Dumper($imdb);
-		print_msg($a);
 		return;
 	}
 }
