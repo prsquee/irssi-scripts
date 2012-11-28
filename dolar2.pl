a2a3d1183407eaf7890722e2367223a867fa8719
prsquee <sq@fnord.com.ar>
2012-11-28 00:28:22 -0300

    fix pesos

--- dolar2.pl
+++ dolar2.pl
@@ -1,42 +1,51 @@
 #dolar2.pl
+#TODO: save the prices. do a lastcheck
+
 use Irssi qw(command_bind signal_add print active_win server_find_tag ) ;
 use LWP::UserAgent;
 use strict;
 sub msg_pub {
 	my($server, $text, $nick, $mask,$chan) = @_;
-	do_dolar($text, $chan, $server) if ($server->{tag} =~ /3dg|fnode|lia|gsg/ and $text =~ /^!(dolar|pesos)( \d+)?$/);
-	#do_dolar($text, $chan, $server) if ($server->{tag} =~ /3dg|fnode|lia|gsg/ and $text =~ /^!pesos \d+$/);
+	do_dolar($text, $chan, $server) if ($server->{tag} =~ /3dg|fnode|lia|gsg/ and $text =~ /^!(?:dolar|pesos)(?:\s+\d+)?$/);
 }
 
 sub do_dolar {
 	my ($text, $chan, $server) = @_;
-  my $blue    = do_fetch("http://www.preciodolarblue.com.ar");
+	my ($ask, $howmuch) = $text =~ /^!(\w+)\s*(\d*)$/;
+  if ($ask eq 'pesos' and not $howmuch) {
+    sayit($server, $chan, "!pesos <CANTIDAD>");
+		return;
+  }
+
+  my $blue = do_fetch("http://www.preciodolarblue.com.ar");
   my $oficial = do_fetch("http://www.cotizacion-dolar.com.ar");
 
 	my ($dolarC, $dolarV) = $oficial =~ /\bDolar\b\s(\d.\d\d)\s(\d\.\d\d)/;
 	my ($euroC, $euroV) = $oficial =~ /Euro\s(\d\.\d\d)\s(\d\.\d\d)/;
   my ($blueC, $blueV) = $blue =~ /\bCompra: Venta: (\d\.\d\d) (\d\.\d\d)\b/;
 
-  #print_msg("$blueCompra - $blueVenta");
-		
-  unless ($dolarC and $dolarV) {
-    sayit($server, $chan, "no encontre los precios D:");
+  unless (($dolarC and $dolarV) or ($blueC and $blueV)) {
+    sayit($server, $chan, "no encontre los precios. check the sauces!");
 		return;
 	}
+#  unless ($blueC and $blueV) {
+#    sayit($server, $chan, "no encontre los precios blue D:");
+#    return;
+#	}
 	my $output = "[Dolar Oficial] => $dolarC/$dolarV | [Dolar Blue] => $blueC/$blueV | [Euro] => $euroC/$euroV";
 
-	my ($ask, $howmuch) = $text =~ /^!(\w+) (\d+)$/;
-	if ($ask == 'dolar' and not defined($howmuch)) {
+
+	if ($ask eq 'dolar' and not $howmuch) {
 		sayit($server, $chan, $output);
 		return;
 	}
-	if ($ask == 'dolar' and $howmuch) {
+	if ($ask eq 'dolar' and $howmuch > 0) {
 		#print_msg("calcular el dolar en pesos");
-		my $stinkyPesos = "[Oficial] " . eval("$howmuch * $dolarC")  . " pesos" . "| [Blue] " . eval($howmuch * $blueC) . " pesos";
+		my $stinkyPesos = "[Oficial] " . eval("$howmuch * $dolarC")  . " pesos" . " | [Blue] " . eval($howmuch * $blueC) . " pesos";
 		sayit($server, $chan, $stinkyPesos) if ($stinkyPesos and !$@);
 		return;
 	}
-	if ($ask == 'pesos' and $howmuch) {
+	if ($ask eq 'pesos' and $howmuch) {
 		#print_msg("calcular esa cantidad de pesos en dolares");
 		my $dollars = eval("$howmuch / $dolarV");
 		my $blueDollars = eval("$howmuch / $blueV");
@@ -45,10 +54,6 @@ sub do_dolar {
 		sayit($server, $chan, "[Oficial] $dollars dolares | [Blue] $blueDollars dolares") if ($dollars and $blueDollars and !$@);
 		return;
 	}
-  #if ($ask == 'pesos' and not defined($howmuch)) {
-  #  sayit($server, $chan, "!pesos <CANTIDAD>");
-	#	return;
-  #}
 }
 sub do_fetch {
   my $fetchme = shift;
@@ -73,8 +78,6 @@ sub do_fetch {
     #print ("$content");
   return $content;
 }
-
-
 sub sayit {
         my ($server, $target, $msg) = @_;
         $server->command("MSG $target $msg");
