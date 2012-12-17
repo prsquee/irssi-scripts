1d60e0817ef37a536790d9f462086bade93832b7
prsquee <sq@fnord.com.ar>
2012-12-17 14:49:47 -0300

    now best friend with imguraffe

--- reimgur.pl
+++ reimgur.pl
@@ -1,42 +1,39 @@
 #reupload an img to imgur
+#this is gonna live until imgur deprecates this old api
 #
 #
-#
-use Irssi qw(command_bind signal_add print active_win server_find_tag ) ;
+use Irssi qw(signal_add print settings_get_str);
 use strict;
-use WWW::Imgur; 
+use WWW::Imgur;
 use JSON;
-#use Data::Dumper;
+use Data::Dumper;
 
-sub msg_pub {
-	my($server, $text, $nick, $mask,$chan) = @_;
-	if ($server->{tag} =~ /3dg|fnode|lia|gsg/) {
-			my ($url) = $text =~ m{^!imgur (https?://[^ ]+)};
-			if ($text =~ /^!imgur/ and not $url) {
-				sayit($server,$chan,"dame un link a una imagen y lo reuploadeo a imgur!");
-			}
-			reupload($url,$server,$chan) if ($url);
-	}
-}
+signal_add('reimgur','reupload');
+#sub msg_pub {
+#	my($server, $text, $nick, $mask,$chan) = @_;
+#	if ($server->{tag} =~ /3dg|fnode|lia|gsg/) {
+#			my ($url) = $text =~ m{^!imgur (https?://[^ ]+)};
+#			if ($text =~ /^!imgur/ and not $url) {
+#				sayit($server,$chan,"dame un link a una imagen y lo reuploadeo a imgur!");
+#			}
+#			reupload($url,$server,$chan) if ($url);
+#	}
+#}
+my $imgur = WWW::Imgur->new ();
+$imgur->key(settings_get_str('imgurkey'));
+
+my $json = new JSON;
 
 sub reupload {
-	my ($url,$server,$chan) = @_;
-	my $imgur = WWW::Imgur->new ();
-	my $apikey = Irssi::settings_get_str('imgurkey');
-        $imgur->key ($apikey);
-	my $jsreply = $imgur->upload($url) or (sayit($server,$chan,"Upload failed") and return);
-	if ($jsreply) {
-		my $json = new JSON;
-		my $reply = $json->allow_nonref->decode($jsreply);
-		#print_msg(Dumper($reply));
+	my ($server,$chan,$url) = @_;
+	my $success = $imgur->upload($url) or (sayit($server,$chan,"Upload failed") and return);
+	if ($success) {
+		my $reply = $json->allow_nonref->utf8->decode($success);
 		my $link = $reply->{'upload'}->{'links'}->{'original'};
-		sayit($server,$chan,$link) if ($link);
+		sayit($server,$chan,"[IMGUR] $link") if ($link);
 	}
 }
-sub sayit { 
+sub sayit {
 	my ($server, $target, $msg) = @_;
 	$server->command("MSG $target $msg");
-}                                         
-sub print_msg { active_win()->print("@_"); }
-signal_add("message public","msg_pub");
-Irssi::settings_add_str('imgur','imgurkey','');
+}
