55a3cfabfe043341d74557dbf28e6fd0680b70b1
prsquee <sq@fnord.com.ar>
2012-12-17 07:03:44 -0300

    json

--- youtube.pl
+++ youtube.pl
@@ -1,12 +1,31 @@
 #check youtube title and desc
+
 use Irssi qw (signal_add print settings_get_str );
 use warnings;
 use strict;
 use IO::Socket::INET; #TODO this is so ugly, I need to use json asap
+use Data::Dumper;
+use LWP::UserAgent;
+use JSON;
+
+#init 
+my $json = new JSON;
+my $ua = new LWP::UserAgent;
+$ua->timeout(10);
+$ua->agent(settings_get_str('myUserAgent'));
 
 sub fetch_tubes {
 	my($server,$chan,$vid) = @_;
-  my ($title, $desc, $time, $views) = get_title($vid) if ($vid);
+  my $url = "http://gdata.youtube.com/feeds/api/videos/$vid?&v=2&alt=json";
+  my $req = $ua->get($url);
+  my $result = $json->utf8->decode($req->decoded_content)->{entry};
+  #print (CRAP Dumper($decoded_json));
+  my $title  = $result->{title}->{'$t'};
+  my $desc   = $result->{'media$group'}->{'media$description'}->{'$t'};
+  my $time   = $result->{'media$group'}->{'yt$duration'}->{seconds};
+  my $views  = $result->{'yt$statistics'}->{'viewCount'};
+
+  #my ($title, $desc, $time, $views) = get_title($vid) if ($vid);
   if($title) {
     if ($time < 10) {
       $time = "[0:0${time}]";
@@ -26,32 +45,6 @@ sub fetch_tubes {
   } else { return; }
 }
 
-# http://code.google.com/apis/youtube/2.0/developers_guide_protocol.html
-sub get_title {
-	my $vid = shift;
-
-	my $sock = IO::Socket::INET->new( 
-		PeerAddr=>'gdata.youtube.com',
-		PeerPort=>'http(80)',
-		Proto=>'tcp',
-	);
-	
-	my $req = "GET /feeds/api/videos/$vid HTTP/1.0\r\n";
-	$req .= "host: gdata.youtube.com\r\n";
-	$req .= "user-agent: UserAgent 1.0\r\n";
-	$req .= "\r\n";
-	print $sock $req;     #oh the humanity T_T
-
-	my $title; my $desc; my $time; my $views;
-	while(<$sock>) {
-		($title) = $_ =~ /<media:title type='plain'>(.*?)<\/media:title>/;
-		($desc) = $_ =~ /<media:description type='plain'>(.*?)<\/media:description>/;
-		($time) = $_ =~ /<yt:duration seconds='(\d+)'/;
-		($views) = $_ =~ /viewCount='(\d+)'/;
-	}
-	close $sock;
-	return ($title,$desc,$time,$views);
-}
 sub sayit {
 	my ($server, $target, $msg) = @_;
 	$server->command("MSG $target $msg");
