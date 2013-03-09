7dd5b945ac0e3898a268f3003e1f67dc89377adb
sQuEE <git@fnord.com.ar>
2013-03-09 01:11:47 -0300

    omdb api update

--- imdb.pl
+++ imdb.pl
@@ -1,21 +1,4 @@
-#imdb api http://imdbapi.com/
-
-#$imdb struct
-#'ID' => 'tt0290978',
-#'Director' => 'N/A',
-#'Poster' => 'http://ia.media-imdb.com/images/M/MV5BMTQ2MzAxODI3M15BMl5BanBnXkFtZTcwNTE2MjAwMQ@@._V1._SX320.jpg',
-#'Actors' => 'Ricky Gervais, Martin Freeman, Mackenzie Crook, Lucy Davis',
-#'Runtime' => '29 mins',
-#'Genre' => 'Comedy, Drama',
-#'Rating' => '9.2',
-#'Released' => '23 Jan 2003',
-#'Response' => 'True',
-#'Votes' => '16626',
-#'Title' => 'The Office',
-#'Year' => '2001',
-#'Rated' => 'TV-MA',
-#'Writer' => 'N/A',
-#'Plot' => 'The story of an office that faces closure when the company decides to downsize
+#omdb api http://omdbapi.com/
 
 use Irssi qw(signal_add print signal_emit) ;
 use strict;
@@ -29,53 +12,51 @@ my $ua = new LWP::UserAgent;
 $ua->timeout(10);
 
 sub do_imdb {
-	my ($server, $chan, $text) = @_;
-  #my ($text, $chan, $server) = @_;
-	my $param; my $query;
+  my ($server, $chan, $text) = @_;
+  my $param = '';
+  my $query = '';
 
-	if ($text =~ /^tt/) {
-		$param = 'i';
-	} elsif ($text =~ /^!imdb/) {
-		$param = 't';
-	}
-		
-	if ($text =~ /^!imdb\s+(.*)/) {
-		$query = $1;
-		$query = uri_escape($query);
-	} elsif ($text =~ /^tt\d+/) {
-		$query = $text;
-	}
-	return if (!$query);
-	my $url = "http://www.imdbapi.com/?${param}=${query}" if ($param and $query);
-	
-	my $got = $ua->get($url);
-	my $content = $got->decoded_content;
-	my $imdb = $json->allow_nonref->decode($content);
+  if ($text =~ /^tt\d+/) {
+    $param = 'i';
+    $query = $text;
+  } elsif ($text =~ /^!imdb\s+(.*)/) {
+    $param = 't';
+    $query = $1;
+    $query = uri_escape($query);
+  }
+    
+  return if (!$query);
+  my $url = "http://www.omdbapi.com/?${param}=${query}" if ($param and $query);
+  
+  my $got = $ua->get($url);
+  my $content = $got->decoded_content;
+  my $imdb = $json->allow_nonref->decode($content);
   #print (CRAP Dumper($imdb));
-	if ($imdb->{Response} !~ /True/ ) {
-		sayit($server,$chan,"not found, try with the full name");
-		return;
-	}
-	my $link;
-	if ($text =~ /^!imdb/) {
-		$link = "- [http://www.imdb.com/title/$imdb->{ID}]";
-	} else {
-		$link = '';
-	}
 
-	my $title = "Title: [$imdb->{Title}] $link";
-	my $genre = "Genre: $imdb->{Genre} - Rating: $imdb->{Rating} - Year: $imdb->{Year}";
-	my $plot = "Plot: $imdb->{Plot}";
-	sayit($server,$chan,$title);
-	sayit($server,$chan,$genre);
-	sayit($server,$chan,$plot);
+  if ($imdb->{Response} !~ /True/ ) {
+    sayit($server,$chan,"not found, try with the full name");
+    return;
+  }
+  my $link;
+  if ($text =~ /^!imdb/) {
+    $link = "- [http://www.imdb.com/title/$imdb->{imdbID}]";
+  } else {
+    $link = '';
+  }
+
+  my $title = "$imdb->{Title} - Directed by $imdb->{Director}";
+  my $genre = "Year: $imdb->{Year} - Genre: $imdb->{Genre} - Rating: $imdb->{imdbRating} - Votes: $imdb->{imdbVotes}";
+  my $plot  = "Plot: $imdb->{Plot} $link";
+  sayit($server,$chan,$title);
+  sayit($server,$chan,$genre);
+  sayit($server,$chan,$plot);
   signal_emit('write to file', "$title\n$genre\n$plot\n") if ($chan =~ /sysarmy|moob/);
-	return;
+  return;
 }
 
 sub sayit {
-	my ($server, $target, $msg) = @_;
-	$server->command("MSG $target $msg");
+  my ($server, $target, $msg) = @_;
+  $server->command("MSG $target $msg");
 }
 signal_add("search imdb","do_imdb");
 
