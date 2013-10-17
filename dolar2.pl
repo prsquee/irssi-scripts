570f8e5394ad20c71842238dd00743651bfca39f
prsquee <sq@fnord.com.ar>
2013-10-17 16:48:03 -0300

    2h buffer and cosmetics

--- dolar2.pl
+++ dolar2.pl
@@ -8,58 +8,61 @@ use strict;
 use warnings;
 use Data::Dumper;
 use JSON;
-use POSIX qw( strftime );
 
 #{{{ init and stuff
 
-my $libreC;
-my $libreV;
-my $blueC;
-my $blueV;
-my $dateFetched;
+my $libreC  = '0';
+my $libreV  = '0';
+my $blueC   = '0';
+my $blueV   = '0';
+my $last_fetch = '0';
+my $bufferme = '7200';
+my $pesos_mugrosos = undef;
+my $sweet_dollars = undef;
 
 my $blueURL   = qw( http://www.eldolarblue.net/getDolarBlue.php?as=json );
 my $libreURL  = qw( http://www.eldolarblue.net/getDolarLibre.php?as=json );
 
-sub init {
+sub getPrice {
   #initial fetch and they'll stay in ram
   ($blueC, $blueV) = gimmeMoney($blueURL);
   ($libreC, $libreV) = gimmeMoney($libreURL);
   #print (CRAP "blues $blueC and $blueV");
   #print (CRAP "libres $libreC and $libreV");
 
-  $dateFetched = strftime "%F", localtime if ($libreC and $libreV and $blueC and $blueV);
-  #TODO: get the date from json
+  #$dateFetched = strftime "%F", localtime if defined($libreC and $libreV and $blueC and $blueV);
+  $last_fetch = time() if defined($libreC and $libreV and $blueC and $blueV);
 }
 #}}}
 
 #{{{ do_dolar
 sub do_dolar {
 	my ($server,$chan,$text) = @_;
-  my ($ask, $howmuch) = $text =~ /^!(\w+)\s*(\d*)$/;
-  if ($ask eq 'pesos' and not $howmuch) {
+  my ($ask, $how_much) = $text =~ /^!(\w+)\s*(\d*)$/;
+  if ($ask eq 'pesos' and not $how_much) {
     sayit($server, $chan, "!pesos <CANTIDAD>");
 		return;
   }
-  init() if ($dateFetched ne strftime("%F",localtime));
+  getPrice() if (time() - $last_fetch > $bufferme); 
 
   my $output = "[Oficial] => $libreC/$libreV | [Blue] => $blueC/$blueV";
 
-	if ($ask eq 'dolar' and not $howmuch) {
+	if ($ask eq 'dolar' and not $how_much) {
 		sayit($server, $chan, $output);
 		return;
 	}
-	if ($ask eq 'dolar' and $howmuch > 0) {
+	if ($ask eq 'dolar' and $how_much > 0) {
 		#print_msg("calcular el dolar en pesos");
-		my $stinkyPesos = "[Oficial] " . eval("$howmuch * $libreC")  . " pesos" . " | [Tarjeta] " . sprintf("%.2f", eval("$howmuch * $libreC * 1.20")) . " pesos" . " | [Blue] " . eval($howmuch * $blueC) . " pesos";
-		sayit($server, $chan, $stinkyPesos) if ($stinkyPesos and !$@);
+		$pesos_mugrosos = "[Oficial] " . eval("$how_much * $libreC")                         . " pesos | ";
+    $pesos_mugrosos.= "[Tarjeta] " . sprintf("%.2f", eval("$how_much * $libreC * 1.20")) . " pesos | ";
+    $pesos_mugrosos.= "[Blue] "    . eval($how_much * $blueC)                            . " pesos";
+		sayit($server, $chan, $pesos_mugrosos) if ($pesos_mugrosos and !$@);
 		return;
 	}
-	if ($ask eq 'pesos' and $howmuch) {
-		my $dollars     = sprintf("%.2f", eval("$howmuch / $libreV"));
-	  my $blueDollars = sprintf("%.2f", eval("$howmuch / $blueV" ));
-	  my $tarjeta     = sprintf("%.2f", eval("$howmuch / $blueV " ));
-		sayit($server, $chan, "[Oficial] $dollars | [Blue] $blueDollars") if ($dollars and $blueDollars and !$@);
+	if ($ask eq 'pesos' and $how_much) {
+    $sweet_dollars = "[Oficial] " . sprintf("%.2f", eval("$how_much / $libreV")) . " | ";
+    $sweet_dollars.= "[Blue] "    . sprintf("%.2f", eval("$how_much / $blueV" ));
+		sayit($server, $chan, $sweet_dollars) if (defined($sweet_dollars) and !$@);
 		return;
 	}
 }
@@ -78,7 +81,9 @@ sub gimmeMoney {
   my $result = $req->content;
   #print (CRAP $result);
   my ($compra, $venta) = $result =~ m{buy:(\d\.\d{4}),sell:(\d\.\d{4})};
-  return ($compra, $venta);
+  
+  (defined($compra) or defined($venta)) ? return ($compra,$venta) : return (0,0);
+  #return ($compra, $venta);
 }#}}}
 #{{{ signal and stuff
 sub sayit {
@@ -87,4 +92,6 @@ sub sayit {
 }
 signal_add("showme the money","do_dolar");
 #}}}
-init();
+
+#do a fetch when the script is loaded.
+getPrice();
