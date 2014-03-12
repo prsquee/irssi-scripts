271b7fd337bf5bca73bd3723e64a096d9b750534
prsquee <sq@fnord.com.ar>
2014-03-12 01:07:23 -0300

    small fixes

--- ttstream.pl
+++ ttstream.pl
@@ -7,10 +7,12 @@ use Data::Dumper;
 use HTML::Entities;
 
 my $sysarmy = '57440969';
+my $nerdear = '2179429297';
 my $sqbot   = '324991882';
 #my $prsquee = '31968735';
 my $chan = '#ssqquuee';
 my $out = undef;
+my $bold = '\x02';
 
 my $server = server_find_chatnet("fnode");
 sub show_tweet {
@@ -28,8 +30,12 @@ sub show_tweet {
     }
     if (defined($out)) {
       $out =~ s/\n|\r/ /g;
-      $server->command("MSG #ssqquuee $out")  if ($tweet->{user}{id_str} eq $sqbot);
-      $server->command("MSG #sysarmy $out")   if ($tweet->{user}{id_str} eq $sysarmy);
+      if ($tweet->{user}{id_str} eq $sqbot) {
+        $server->command("MSG #ssqquuee $out");
+      }
+      elsif ($tweet->{user}{id_str} =~ /$sysarmy|$nerdear/) {
+        $server->command("MSG #sysarmy $out"); #if ($tweet->{user}{id_str} eq $sysarmy);
+      }
     }
   }
   #$server->command("MSG $chan [\x02\@$tweet->{user}{screen_name}\x02] $tweet->{text}") 
@@ -40,7 +46,7 @@ our $sysarmyStream = AnyEvent::Twitter::Stream->new(
   token           => settings_get_str('twitter_access_token'),
   token_secret    => settings_get_str('twitter_access_token_secret'),
   method          => "filter",
-  follow          => "$sysarmy, $sqbot",
+  follow          => "$sysarmy, $sqbot, $nerdear",
   on_connect      => sub { print (CRAP "connected to twitter stream.");},
   on_tweet        => \&show_tweet,
   on_eof          => sub { print (CRAP "EOF.") && return;},
--- vimeo.pl
+++ vimeo.pl
@@ -19,9 +19,11 @@ sub check_vimeo {
   $ua->agent(settings_get_str('myUserAgent'));
   $query = $query . $vid . '.json';
   my $req = $ua->get( $query );
-  my $result = eval { $json->utf8->decode($req->decoded_content) };
+  my $result = shift (eval { $json->utf8->decode($req->decoded_content) }); #the result in json comes in an array of 1
   return if $@;
-  print (CRAP Dumper($result));
+
+  #print CRAP Dumper($result);
+
   my $time = $result->{'duration'};
   my $hour = ($time >= 3600) ? sprintf ("%02d:", $time/3600) : undef;
   $time = $time - 3600 * int($hour) if defined($hour);
@@ -30,9 +32,11 @@ sub check_vimeo {
   if ($time >= 60) {
     $mins = sprintf ("%02d:", $time/60);
     $secs = sprintf ("%02d",  $time%60);
-  } elsif ($time < 60) {
+  } 
+  elsif ($time < 60) {
     $secs = sprintf("%02d", $time);
   }
+
   $time = '[';
   $time .= $hour if defined($hour);
   $time .= $mins if defined($mins);
