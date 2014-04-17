17028a269831c2a73cc69833824112c6b22f71e3
prsquee <sq@fnord.com.ar>
2014-04-17 01:42:29 -0300

    cosmetics

--- btce.pl
+++ btce.pl
@@ -1,6 +1,5 @@
 #bitcoins 
 #https://btc-e.com/api/2/btc_usd/ticker
-#https://btc-e.com/api/2/ltc_usd/ticker
 
 use Irssi qw(signal_add print settings_get_str) ;
 use strict;
@@ -12,8 +11,8 @@ use Data::Dumper;
 signal_add('gold digger','bitcoin');
 signal_add('silver digger','litecoin');
 
-my $buffer = 3600;
-my %fetched = ();
+my $wait_for = 3600;
+my %time_fetched = ();
 my $json = new JSON;
 
 #btce
@@ -24,20 +23,20 @@ $ua->timeout(15);
 #{{{ check bitcoin
 sub fetch_price {
   my ($server, $chan, $coin) = @_;
-  my $t = defined($btce{$coin}) ? $fetched{$coin} : 0;
-  if (time - $t > $buffer) {
+  my $last_fetch = defined($btce{$coin}) ? $time_fetched{$coin} : 0;
+  if (time - $last_fetch > $wait_for) {
     $ua->agent(settings_get_str('myUserAgent'));
     my $url = "https://btc-e.com/api/2/${coin}_usd/ticker";
-    my $req = $ua->get($url);
-    my $r = eval { $json->utf8->decode($req->decoded_content) };
+    my $ua_got = $ua->get($url);
+    my $decoded_json = eval { $json->utf8->decode($ua_got->decoded_content) };
     return if $@;
-    if ($r and not $@) {
+    if ($decoded_json) {
       $btce{$coin}  = '[BTCe] ';
-      $btce{$coin} .= 'sell: $'     . sprintf("%.2f", $r->{ticker}{sell}) .' | ';
-      $btce{$coin} .= 'buy: $'      . sprintf("%.2f", $r->{ticker}{buy} ) .' | ';
-      $btce{$coin} .= 'highest: $'  . sprintf("%.2f", $r->{ticker}{high}) .' | ';
-      $btce{$coin} .= 'average: $'  . sprintf("%.2f", $r->{ticker}{avg} )       ;
-      $fetched{$coin} = time();
+      $btce{$coin} .= 'sell: $'     . sprintf("%.2f", $decoded_json->{ticker}{sell}) .' | ';
+      $btce{$coin} .= 'buy: $'      . sprintf("%.2f", $decoded_json->{ticker}{buy} ) .' | ';
+      $btce{$coin} .= 'highest: $'  . sprintf("%.2f", $decoded_json->{ticker}{high}) .' | ';
+      $btce{$coin} .= 'average: $'  . sprintf("%.2f", $decoded_json->{ticker}{avg} )       ;
+      $time_fetched{$coin} = time();
     } else { $btce{$coin} = undef; }
   }
   sayit ($server, $chan, $btce{$coin}) if (defined($btce{$coin}));
@@ -48,4 +47,3 @@ sub bitcoin   { fetch_price(@_, 'btc'); }
 sub litecoin  { fetch_price(@_, 'ltc'); }
 
 sub sayit { my $s = shift; $s->command("MSG @_"); }
-
