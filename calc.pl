f83f0dcfc7aee1adfb9b252396a5c82e174a0652
prsquee <sq@fnord.com.ar>
2014-07-31 23:00:35 -0300

    leading symbols strip

--- calc.pl
+++ calc.pl
@@ -1,30 +1,29 @@
 #calc.pl
+#TODO: add cientific stuff 
 use warnings;
 use strict;
 use Irssi qw( signal_add print );
 
-
 my $error = 'INSUFFICIENT DATA FOR MEANINGFUL ANSWER';
 
 sub do_calculate {
-	my ($server, $chan, $text) = @_;
-  #if ($server->{tag} =~ /3dg|fnode|lia|gsg/ and $msg =~ /^!calc/) {
-  $text =~ s/^!calc //;
+  my ($server, $chan, $text) = @_;
+  $text =~ s/^!calc +//;
   $text =~ s/,/./g;
   $text =~ s/[^*.+0-9&|)(x\/^-]//g;   # remove anything that is not a math symbol
-  $text =~ s/\*\*/^/g;				  # to teh powah!
-  $text =~ s/([*+\\.\/x-])\1*/$1/g;	  # borrar simbolos repetidos
+  $text =~ s/\*\*/^/g;                # to teh powah!
+  $text =~ s/([*+\\.\/x-])\1*/$1/g;   # borrar simbolos repetidos
   $text =~ s/\^/**/g;                 #TODO arreglar el regex de ariiba
   $text =~ s/(?<!0)x//g;              #stripar hex con lookback
+  $text =~ s/^\W+//;                  #leading useless stuff
 
   my $answer = eval("($text) || '0'");
-  $answer = "I can't hold all these numbers :(" if (defined($answer) and $answer eq 'inf');
-			
+  $answer = "calculating... might take a while." if ($answer eq 'inf');
   my $out = $@ ? $error : $answer;
   sayit($server,$chan,$out);
 }
 sub sayit {
-	my ($server, $target, $msg) = @_;
-	$server->command("MSG $target $msg");
+  my ($server, $target, $msg) = @_;
+  $server->command("MSG $target $msg");
 }
 signal_add("calculate","do_calculate");
