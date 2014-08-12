f7cf72d6d0817dd983dba357905a8b99819f9556
prsquee <sq@fnord.com.ar>
2014-08-12 00:35:38 -0300

    new api from blockio

--- blockio.pl
+++ blockio.pl
@@ -1,46 +1,63 @@
 #bitcoins 
-#https://www.bitstamp.net/api/ticker/
-
-use Irssi qw(signal_add print settings_get_str) ;
+#https://block.io/api
+use Irssi qw(signal_add print settings_get_str settings_add_str) ;
 use strict;
 use warnings;
 use JSON;
 use LWP::UserAgent;
 use Data::Dumper;
  
-signal_add('gold digger','bitstamp');
+signal_add('gold digger','check_bitcoins');
+settings_add_str('bot config', 'bitcoin_apikey', '');
 
-my $buffer = 1800;
-my $fetched = undef;
 my $json = new JSON;
+my $ua  = LWP::UserAgent->new(timeout => 15);
 
-my $bitstamp    = undef;
-my $url = 'https://www.bitstamp.net/api/ticker';
-my $ua    = new LWP::UserAgent;
-$ua->timeout(15);
+my $prices_ref   = undef;
+my $buffered_for = 1800;  #30m
+my $last_fetch   = 0;
 
+my $api_key = settings_get_str('bitcoin_apikey');
+my $price_base = 'USD';
 
-sub bitstamp {
+my $apiurl = 'https://block.io/api/v1/get_current_price/?'
+           . "api_key=$api_key"
+           . '&'
+           . "price_base=$price_base";
+
+sub check_bitcoins {
   my ($server, $chan) = @_;
-  #if (time - $fetched{$bitstamp} > 60 or not defined($bitstamp)) {
-  my $t = defined($bitstamp) ? $fetched : 0;
-  if (time - $t > $buffer) {
-    $ua->agent(settings_get_str('myUserAgent'));
-    my $req = $ua->get($url);
-    my $r = eval { $json->utf8->decode($req->decoded_content) };
-    return if $@;
-    if ($r and not $@) {
-      $bitstamp  = '[Bitstamp] ';
-      $bitstamp .= 'high: $' . $r->{high} . ' | ';
-      $bitstamp .= 'low: $' .  $r->{low}  . ' | ';
-      $bitstamp .= 'average: $' . sprintf("%.2f", eval("($r->{bid} + $r->{ask}) / 2"));
-      $fetched = time();
-    } else { $bitstamp = undef; }
-    #print (CRAP Dumper($r)) 
+  my $output = undef;
+
+  $prices_ref = fetch_prices() if (time - $last_fetch > $buffered_for);
+
+  if ($prices_ref) {
+    foreach my $price (@{$prices_ref}) {
+      #print (CRAP $price->{'exchange'});
+      $output .= "[$price->{'exchange'}] "
+              . '$'
+              . sprintf("%.2f", $price->{'price'})
+              . ' :: ';
+    }
+    sayit ($server, $chan, $output);
   }
-  sayit ($server, $chan, $bitstamp) if (defined($bitstamp));
-  return;
+  else {
+    sayit ($server, $chan, "sorry, cant check prices now.");
+    return;
+  }
 }
 
+sub fetch_prices {
+  $ua->agent(settings_get_str('myUserAgent'));
+  my $raw_results = $ua->get($apiurl)->decoded_content;
+  my $decoded_json = $json->utf8->decode($raw_results);
+  if ($decoded_json->{'status'} eq 'success') { 
+    $last_fetch = time();
+    return $decoded_json->{'data'}->{'prices'};
+  } 
+  else {
+    return undef;
+  }
+}
 sub sayit { my $s = shift; $s->command("MSG @_"); }
 
