e89e6c3f1418014b0140ab8ae2b9a6fb1eda4a59
sQuEE <fede@fnord.com.ar>
2015-02-12 00:50:35 -0300

    can search by year

--- imdb.pl
+++ imdb.pl
@@ -6,58 +6,61 @@ use URI::Escape qw( uri_escape );
 use Data::Dumper;
 use JSON;
 
+signal_add("search imdb","do_imdb");
+
 my $json = new JSON;
 my $ua = new LWP::UserAgent;
 $ua->timeout(10);
 
 sub do_imdb {
   my ($server, $chan, $text) = @_;
-  my $param = '';
-  my $query = '';
+  my $param = undef;
+  my $query = undef;
+  my $year  = undef;
 
   if ($text =~ /^tt\d+/) {
     $param = 'i';
     $query = $text;
-  } elsif ($text =~ /^!imdb\s+(.*)/) {
-    $param = 't';
+  } 
+  elsif ($text =~ /^!imdb\s+(.*)$/) { 
     $query = $1;
+    if ($query =~ /(19|20\d{2})$/) {
+      $year = $1;
+      $query =~ s/ \d{4}$//;
+    }
     $query = uri_escape($query);
+    $param = 't';
   }
-  if (!$query) {
-    sayit($server,$chan,"I need a movie title");
+  unless ($query) {
+    sayit($server, $chan, 'I need a movie title');
     return;
   }
-  my $url = "http://www.omdbapi.com/?${param}=${query}" if ($param and $query);
+  my $url = "http://www.omdbapi.com/?${param}=${query}" if $param and $query;
+  $url .= "&y=${year}" if $year;
+
   my $got = $ua->get($url);
   my $content = $got->decoded_content;
   my $imdb = eval { $json->allow_nonref->decode($content) };
   return if $@;
-  #print (CRAP Dumper($imdb));
 
   if ($imdb->{Response} !~ /True/ ) {
-    sayit($server,$chan,"not found, try with the full name");
+    sayit($server, $chan, 'not found, try with the full name.');
     return;
   }
-  my $link;
-  if ($text =~ /^!imdb/) {
-    $link = "- [http://www.imdb.com/title/$imdb->{imdbID}]";
-  } else {
-    $link = '';
-  }
+  my $link = ($text =~ /^!imdb/) ? 'http://www.imdb.com/title/' . $imdb->{imdbID} 
+                                 : undef;
+  my $title = "$imdb->{Title} [$imdb->{Year}] - $imdb->{Genre} - " 
+            . "Directed by $imdb->{Director}";
 
-  my $title = "$imdb->{Title} [$imdb->{Year}] - $imdb->{Genre} - Directed by $imdb->{Director}";
   my $actor = "Actors: $imdb->{Actors}";
-  my $plot  = "Plot: $imdb->{Plot} $link";
-  sayit($server,$chan,$title);
-  sayit($server,$chan,$actor);
-  sayit($server,$chan,$plot);
-  signal_emit('write to file', "$title\n$actor\n$plot\n") if ($chan =~ /sysarmy|moob/);
+  my $plot  = "Plot: $imdb->{Plot}";
+  $plot .= ($link) ? " - [${link}]" : '';
+
+  sayit($server, $chan, $title);
+  sayit($server, $chan, $actor);
+  sayit($server, $chan, $plot);
   return;
 }
 
-sub sayit {
-  my ($server, $target, $msg) = @_;
-  $server->command("MSG $target $msg");
-}
-signal_add("search imdb","do_imdb");
+sub sayit { my $s = shift; $s->command("MSG @_"); }
 
