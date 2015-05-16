3d075ec7f872bf6fced6aa48c6c118729b3dcbeb
prsquee <github@fnord.com.ar>
2015-05-15 21:41:16 -0300

    youtube v3 api

--- youtube.pl
+++ youtube.pl
@@ -1,4 +1,6 @@
-#check youtube title and desc
+#youtube stuff
+#doc https://developers.google.com/youtube/v3/getting-started
+
 use Irssi qw (signal_add signal_emit print settings_get_str );
 use warnings;
 use strict;
@@ -8,27 +10,40 @@ use JSON;
 
 #init 
 signal_add("check tubes", "fetch_tubes"); 
+Irssi::settings_add_str('apikey', 'youtube_apikey', '');
 
 my %fetched_vids = ();
-my $json = JSON->new();
-my $ua   = LWP::UserAgent->new( timeout => '15' );
-my $api_url = 'http://gdata.youtube.com/feeds/api/videos/'; 
+my $json    = JSON->new();
+my $ua      = LWP::UserAgent->new( timeout => '15' );
+
+my $api_url = 'https://www.googleapis.com/youtube/v3/videos?id=';
+my $api_key = '&key=' . settings_get_str('youtube_apikey');
+my $part    = '&part=contentDetails,snippet,statistics';
+my $field   = '&fields=items('
+                          . 'contentDetails(duration),'
+                          . 'snippet(title),'
+                          . 'statistics(viewCount)'
+                          . ')';
 
 sub fetch_tubes {
   my($server, $chan, $vid) = @_;
 
   if (not exists($fetched_vids{$vid})) {
-    my $url = $api_url . $vid . '?&v=2&alt=json';
+    my $request_url = $api_url . $vid . $api_key . $part . $field ;
+
     $ua->agent(settings_get_str('myUserAgent'));
-    my $req = $ua->get($url);
-    my $result = eval { $json->utf8->decode($req->decoded_content)->{entry} };
+    my $req = $ua->get($request_url);
+    my $result = eval { $json->utf8->decode($req->decoded_content) };
     return if $@;
 
-    my $title = $result->{title}->{'$t'};
-    my $desc  = $result->{'media$group'}->{'media$description'}->{'$t'};
-    my $time  = $result->{'media$group'}->{'yt$duration'}->{seconds};
-    my $views = $result->{'yt$statistics'}->{'viewCount'};
+    #items comes as an one element array.
+    my $item = shift @{ $result->{'items'} };
 
+    my $title = $item->{'snippet'}->{'title'};
+    my $time  = $item->{'contentDetails'}->{'duration'};
+    my $views = $item->{'statistics'}->{'viewCount'};
+
+    #print (CRAP "$title - $time - $views");
     if ($title) {
       #print (CRAP $time);
       $time = format_time($time);
@@ -44,27 +59,16 @@ sub fetch_tubes {
 
 sub format_time {
   my $time = shift;
-  my $hour = undef;
-  my $mins = '00';
-  my $secs = 0;
 
-  if ($time > 0) {
-    $hour = sprintf ("%02d", $time/3600) if $time >= 3600;
-    $time = $time - 3600 * int($hour) if $hour;
-    if ($time >= 60) {
-      $mins = sprintf ("%02d", $time/60);
-      $secs = sprintf ("%02d", $time%60);
-    } 
-    elsif ($time < 60) {
-      $secs = sprintf("%02d", $time);
-    }
-    $time = "${mins}:${secs}";
-    $time = "${hour}:${time}" if ($hour);
-    $time = "[${time}]";
-  } 
-  else {
-    $time = '[LIVE]';
-  }
+  #time comes in iso 8601 duration format
+  #like PT2H11M12S, if the vid is longer than a day: P2DT2H11M12S
+  #we match all the integers and start from the last element, seconds.
+  my @integers = $time =~ /(\d+)/g;
+  
+  $time = join(':', map { sprintf("%02d", $_) } @integers);
+
+  $time = 'live' if $time eq '00';
+  $time = '[' . $time . ']';
   return $time;
 }
 sub sayit { my $s = shift; $s->command("MSG @_"); }
