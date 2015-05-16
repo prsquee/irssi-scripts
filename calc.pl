071ff56edb8d014a8604503ff8d90470fac5a0f8
prsquee <github@fnord.com.ar>
2015-05-15 22:05:44 -0300

    fixed everything

--- calc.pl
+++ calc.pl
@@ -7,23 +7,20 @@ use Irssi qw( signal_add print );
 my $error = 'INSUFFICIENT DATA FOR MEANINGFUL ANSWER';
 
 sub do_calculate {
-  my ($server, $chan, $text) = @_;
-  $text =~ s/^!calc +//;
-  $text =~ s/,/./g;
-  $text =~ s/[^*.+0-9&|)(x\/^-]//g;   # remove anything that is not a math symbol
-  $text =~ s/\*\*/^/g;                # to teh powah!
-  $text =~ s/([*+\\.\/x-])\1*/$1/g;   # borrar simbolos repetidos
-  $text =~ s/\^/**/g;                 #TODO arreglar el regex de ariiba
-  $text =~ s/(?<!0)x//g;              #stripar hex con lookback
-  $text =~ s/^\W+//;                  #leading useless stuff
+  my ($server, $chan, $math) = @_;
+  $math =~ s/^!calc +//;
+  $math =~ s/,/./g;
+  $math =~ s/[^*.+0-9&|)(x\/^-]//g;   # remove anything that is not a math symbol
+  $math =~ s/\*\*/^/g;                # to teh powah!
+  $math =~ s/([*+\\.\/x-])\1*/$1/g;   # borrar simbolos repetidos
+  $math =~ s/\^/**/g;                 #TODO arreglar el regex de ariiba
+  $math =~ s/(?<!0)x//g;              #stripar hex con lookback
+  $math =~ s/^\W+//;                  #leading useless stuff
 
-  my $answer = eval("($text) || '0'");
-  $answer = 'fourty-two' if ($answer eq 'inf');
-  my $out = $@ ? $error : $answer;
-  sayit($server,$chan,$out);
+  my $answer = eval($math);
+  #print (CRAP "$answer and $math");
+  $answer = $error if (not $answer or $answer eq 'inf');
+ sayit($server, $chan, $answer);
 }
-sub sayit {
-  my ($server, $target, $msg) = @_;
-  $server->command("MSG $target $msg");
-}
+sub sayit { my $s = shift; $s->command("MSG @_"); }
 signal_add("calculate","do_calculate");
