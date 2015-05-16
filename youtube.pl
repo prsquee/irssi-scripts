f88eb62e89cacc1c849287d284e386baa34e934e
prsquee <github@fnord.com.ar>
2015-05-15 21:46:06 -0300

    add 00 minutes if the video is under one minute.

--- youtube.pl
+++ youtube.pl
@@ -68,6 +68,7 @@ sub format_time {
   $time = join(':', map { sprintf("%02d", $_) } @integers);
 
   $time = 'live' if $time eq '00';
+  $time = '00:' . $time if $time !~ /:/;
   $time = '[' . $time . ']';
   return $time;
 }
