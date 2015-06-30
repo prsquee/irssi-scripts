f9f1fc6cc10bcd2d2d8edd14d1cf569ae9a83a24
prsquee <github@fnord.com.ar>
2015-06-30 11:22:13 -0300

    cosmetics.

--- replace.pl
+++ replace.pl
@@ -3,7 +3,7 @@ use Irssi qw( command_bind signal_add print settings_get_str ) ;
 use warnings;
 use strict;
 
-my %lastline;
+my %lastline_of;
 my $delims = q(/|!@#$%&:;);
 my $regex = qr{(?x-sm:
     ^s
@@ -19,38 +19,35 @@ my $regex = qr{(?x-sm:
 
 my $networks = settings_get_str('active_networks');
 
-sub msg_pub {
-  my ($server,$text,$nick,$mask,$chan) = @_;
+sub search_and_replace {
+  my ($server, $text, $nick, $mask, $chan) = @_;
   return if ($server->{tag} !~ /$networks/);
   return if ($text =~ /^!/);
   if ($text =~ $regex) {
-    my $search;
+    my $search = undef;
     my $replace = $3;
     use re 'eval';
     eval { $search = qr/$2/; };
-    if ($@) { sayit($server,$chan, "your regex is bad and you should feel bad"); return; }
+    if ($@) {
+      sayit($server, $chan, 'your regex is bad and you should feel bad.');
+      return;
+    }
+    my $another_user = $4 if $4;
+    my $this_user = ($another_user) ? $another_user . $mask : $nick . $mask;
+    my $replaced = $lastline_of{$this_user};
+    return if (!$replaced);
 
-    my $user = $4 if $4;
-    my $nickToFind;
-    if ($search) {
-      if ($user) { $nickToFind = $user . $server->{tag}; }
-      else { $nickToFind = $nick . $server->{tag}; }
+    if ($replaced =~ s{$search}{$replace}ig) {
+      my $result = ($another_user) ? "$another_user quiso decir: $replaced"
+                                   : "FTFY: $replaced"
+                                   ;
 
-      my $replaced = $lastline{$nickToFind};
-      return if (!$replaced);
-      if ($replaced =~ s{$search}{$replace}ig) {
-        if (!$user) { sayit($server,$chan,"FTFY: $replaced"); }
-        else { sayit($server,$chan,"$user quiso decir: $replaced"); }
-      }
+      sayit($server, $chan, $result);
     }
   }
   else {
-    #si no es s///, guardar la linea
-    $lastline{"$nick"."$server->{tag}"} = $text;
+    $lastline_of{$nick . $mask} = $text;
   }
 }
-sub sayit {
-  my ($server, $target, $msg) = @_;
-  $server->command("MSG $target $msg");
-}
-Irssi::signal_add("message public","msg_pub");
+sub sayit { my $s = shift; $s->command("MSG @_"); }
+Irssi::signal_add('message public','search_and_replace');
