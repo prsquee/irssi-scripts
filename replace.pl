464f1f30954d2f67545e1236d2d1361e2ba3e9b4
prsquee <github@fnord.com.ar>
2015-07-05 00:13:33 -0300

    REGEX POLICE IS HERE.

--- replace.pl
+++ replace.pl
@@ -1,21 +1,27 @@
 #replace.pl
-use Irssi qw( command_bind signal_add print settings_get_str ) ;
+use Irssi qw( command_bind signal_add signal_emit print settings_get_str );
 use warnings;
 use strict;
 
+signal_add('message public','search_and_replace');
+
 my %lastline_of;
-my $delims = q(/|!@#$%&:;);
+my $delimiter = q([/|!@#$%&:;]);
 my $regex = qr{(?x-sm:
     ^s
-      ([$delims])
-        ( (?:\\.|(?!\1).)+ )
-        #matchea \\ and a single char or lookahead un char que no sea un delimiter 
-      \1                       #otro delimiter 
-        ( (?:\\.|(?!\1).)* )
-      \1?                      #cierre opcional
-      (?:\s*\@(\w+))?           #optional username
+      ($delimiter)      # this is the first match, only used as \1.
+        (             # start of the searching pattern. this is \2
+          (?:\\.      # here we match a \ followed by any char, such as \w
+            |
+          (?!\1).)+   # lookahead a char that is not a delimiter
+        )             # end of matching \2
+      \1              # match another delimiter, closing the search pattern.
+        (             # start of the replace pattern, this is \3
+          (?:(?!\1).)*   # we match anything that is not same lookahead but optional
+        )             # end of matching \3
+      \1?             # closing is optional by losing karma.
+      (?:\s*\@(\w+))? # this is \4, used when correcting someone else.
 )};
-#m{^s([/|@])((?:(?!\1).)+)\1((?:.(?!\1).)*)\1?}) {
 
 my $networks = settings_get_str('active_networks');
 
@@ -24,11 +30,14 @@ sub search_and_replace {
   return if ($server->{tag} !~ /$networks/);
   return if ($text =~ /^!/);
   if ($text =~ $regex) {
+    my $used_delimiter = $1;
     my $search = undef;
-    my $replace = $3;
-    my $another_user = $4 if $4;
+    my $replace = ($3) ? $3 : '';
+    my $another_user = ($4) ? $4 : undef;
+
     use re 'eval';
     eval { $search = qr/$2/; };
+
     if ($@) {
       sayit($server, $chan, 'your regex is bad and you should feel bad.');
       return;
@@ -37,6 +46,11 @@ sub search_and_replace {
     my $replaced = $lastline_of{$this_user};
     return if (!$replaced);
 
+    if (not $another_user and $text !~ /$used_delimiter$/) {
+      sayit($server, $chan, 'CLOSE YOUR REGEX! 1 karma taken from ' . $nick);
+      signal_emit('karma bitch', $nick, '--', $chan . '_' . $server->{tag});
+      return;
+    }
     if ($replaced =~ s{$search}{$replace}ig) {
       my $result = ($another_user) ? "$another_user quiso decir: $replaced"
                                    : "FTFY: $replaced"
@@ -44,10 +58,11 @@ sub search_and_replace {
 
       sayit($server, $chan, $result);
     }
+
   }
   else {
+    #we save every line;
     $lastline_of{$nick . $mask} = $text;
   }
 }
 sub sayit { my $s = shift; $s->command("MSG @_"); }
-Irssi::signal_add('message public','search_and_replace');
