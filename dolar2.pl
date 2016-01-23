95cdf89b30d48fbcc0b4d83bddb3c9114b9e78b4
prsquee <github@fnord.com.ar>
2016-01-23 12:21:18 -0300

    no more blue stuff

--- dolar2.pl
+++ dolar2.pl
@@ -11,8 +11,6 @@ use Data::Dumper;
 
 my $oficial_compra  = undef;
 my $oficial_venta   = undef;
-my $blue_compra     = undef;
-my $blue_venta      = undef;
 my $last_fetch      = 0;
 my $bufferme        = '1800';  #30mins
 my $lanacion_url    = 'http://contenidos.lanacion.com.ar/json/dolar';
@@ -26,18 +24,14 @@ sub get_price {
   $raw_result =~ s/,/./g;
   #print (CRAP $raw_result);
 
-  ($oficial_venta, $blue_venta, $oficial_compra, $blue_compra)
-    = $raw_result =~ m{ "CasaCambioVentaValue" :"([^"]+)?"\..+\.
-                        "InformalVentaValue"   :"([^"]+)?"\.
-                        "CasaCambioCompraValue":"([^"]+)?"\.
-                        "InformalCompraValue"  :"([^"]+)?"
+  ($oficial_venta, $oficial_compra)
+    = $raw_result =~ m{ 
+			"CasaCambioVentaValue" :"([^"]+)?"
+			.*
+                        "CasaCambioCompraValue":"([^"]+)?"
                       }x;
 
-  $last_fetch = time() if ( $oficial_compra and 
-                            $oficial_venta  and
-                            $blue_compra    and 
-                            $blue_venta
-                          );
+  $last_fetch = time() if ($oficial_compra and $oficial_venta);
 }
 #}}}
 
@@ -53,14 +47,10 @@ sub do_dolar {
 
   if ($ask =~ /^dol[oa]r$/) {
     if (!$how_much) {
-      my $output = '[Oficial] ';
-      $output .= ($oficial_compra and $oficial_venta)
-                 ? "$oficial_compra | $oficial_venta "
-                 : 'no idea ';
-
-      $output .= ':: [Blue] ';
-      $output .= ($blue_compra and $blue_venta) ? "$blue_compra | $blue_venta"
-                                                : 'no idea';
+      #my $output = '[Oficial] ';
+      my $output = ($oficial_compra and $oficial_venta)
+                 ? 'Compra: ARS $' . $oficial_compra . ' :: Venta: ARS $' . $oficial_venta
+                 : 'no idea';
 
       sayit($server, $chan, $output);
       return;
@@ -68,20 +58,10 @@ sub do_dolar {
     elsif ($how_much > 0) {
       my $pesos = undef;
       $pesos .= ($oficial_compra)
-                ? 'Oficialmente el banco te cambia AR$'
-                  . sprintf("%.2f", eval($how_much * $oficial_compra))
-                  . '. Comprando con la tarjeta te cobran AR$'
-                  . sprintf("%.2f", eval($how_much * $oficial_venta * 1.35))
-                  . ' final. Si no sos pobre, te sale AR$'
-                  . sprintf("%.2f", eval($how_much * $oficial_venta * 1.20))
-                  . ' para comprar dólar ahorro. '
-                : undef;
+              ? 'ARS $' . sprintf("%.2f", eval($how_much * $oficial_compra))
+              : undef;
 
-      $pesos .= ($blue_compra)
-                ? 'En una cueva podés cambiar por AR$'
-                  . sprintf("%.2f", eval($how_much * $blue_compra)) . '.'
-                : undef;
-      $pesos = $pesos || 'not now hooman, try later.';
+      $pesos = $pesos || 'no price found, try later.';
 
       sayit($server, $chan, $pesos) if (!$@);
       return;
@@ -91,23 +71,10 @@ sub do_dolar {
   if ($ask eq 'pesos' and $how_much > 0) {
     my $dollars = undef;
     $dollars .= ($oficial_venta)
-                ? 'Oficialmente son u$'
-                  . sprintf("%.2f", eval($how_much / $oficial_venta))
-                  . '. Podés pagar u$'
-                  . sprintf("%.2f", eval($how_much / ($oficial_venta * 1.35)))
-                  . ' de tu tarjeta. '
-                  . 'Si AFIP te deja, podés comprar u$'
-                  . sprintf("%.2f", eval($how_much / ($oficial_venta* 1.20)))
-                  . ' de ahorro. '
+                ? 'USD $' . sprintf("%.2f", eval($how_much / $oficial_venta))
                 : undef;
 
-    $dollars .= ($blue_venta)
-                ? 'O En una cueva podés comprar u$'
-                  . sprintf("%.2f", eval("$how_much / $blue_venta"))
-                  . '. '
-                : undef;
-
-    $dollars = $dollars || 'not now hooman, try later.';
+    $dollars = $dollars || 'no price found, try later.';
 
     sayit($server, $chan, $dollars) if (!$@);
     return;
