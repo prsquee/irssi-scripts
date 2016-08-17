c242752abb67cd0d2e3c10caf4df809cf4323a36
prsquee <github@fnord.com.ar>
2016-08-17 15:38:22 -0300

    status 599 handling

--- ggl.pl
+++ ggl.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use Irssi qw(signal_add print settings_get_str) ;
 use WWW::Google::URLShortener;
+use utf8;
 
 Irssi::settings_add_str('apikey', 'google_apikey', '');
 
@@ -12,6 +13,7 @@ my $shortener = WWW::Google::URLShortener->new(
 
 sub do_shortme {
   my $url = shift;
-  my $shorten = $shortener->shorten_url($url);
-  return $shorten if ($shorten);
+  my $shorten = eval { $shortener->shorten_url($url) };
+  ($@) ? return 'couldnt make a short url '
+       : return $shorten;
 }
