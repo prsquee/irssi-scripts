5101710a35b833528409f5c1fc2c02234c35b6ab
prsquee <github@fnord.com.ar>
2017-09-12 10:59:53 -0300

    meh. lost the will

--- regex.pl
+++ regex.pl
@@ -7,7 +7,7 @@ my $delims = q(/|!@#$:;);
 my $regex = qr{(?x-sm:
     ^!re
     \s+
-    "([^"]+)"           #our string and thgis is $1 TODO single quotes and other stuff
+    "([^"]+)"           #our string and thingy is $1
     \s+
     ([$delims])         #this will be \2
     ( (?:(?!\2).)+ )    #at least one char that is not our delims this is $3
@@ -21,23 +21,24 @@ my $regex = qr{(?x-sm:
 
 
 sub pub_msg {
-  my ($server,$text,$nick,$mask,$chan) = @_ ;
-  return if ($server->{tag} !~ /3dg|fnode/);
-  sayit($server, $chan,"regex tester, usage: !re \"string to match\" /regex/") if ($text eq "!re");
+  my ($server,$text,$nick,$mask,$chan) = @_;
+  return if ($server->{tag} !~ /fnode/);
+  sayit($server, $chan,"broken and not safe!") if ($text eq "!re");
 
   #if ($text =~ m{!re "([^"]+)" /([^/]+)/$}) {
   if ($text =~ $regex) {
     my $string2match = $1;
     my $regex = $3;
-    my $mods = $4 || 'gi';        #TODO fix this uselessness
-    
+    my $mods = $4 || 'gi';        #TODO I just lost interest
+
     if ($string2match and $regex) {
       use re 'eval';
       eval { my $test = qr/$regex/};
-      sayit($server,$chan,"your regex skill is bad, and you should feel bad") if ($@);
+      sayit($server,$chan,"your regex is bad, and you should feel bad") if ($@);
+      return;
     }
     my @matches;
-    eval { @matches = $string2match =~ /$regex/ig ; };
+    eval { @matches = $string2match =~ /\Q$regex\E/ig ; };
     #print(CRAP Dumper(@matches));
     if (! $@ and scalar(@matches) > 0) {
       my $matched = join(":", @matches);
