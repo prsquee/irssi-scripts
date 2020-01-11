f76f458ba23ce2b1272c43b9cc779869350b4506
prsquee <github@fnord.com.ar>
2020-01-10 23:34:02 -0300

    cleanup

--- dolar3.pl
+++ dolar3.pl
@@ -17,7 +17,7 @@ my $bufferme   = '10';  #30mins
 my $json = JSON->new();
 my $bluelytics_url = 'http://api.bluelytics.com.ar/v2/latest';
 
-sub get_price {
+sub fetch_price {
   my $url = shift;
   my $ua  = LWP::UserAgent->new( timeout => 9 );
   $ua->agent(Irssi::settings_get_str('myUserAgent'));
@@ -25,47 +25,42 @@ sub get_price {
   my $raw_result = $ua->get($url)->content();
   %prices = %{ $json->utf8->decode($raw_result) };
 
-
   $last_fetch = time() if %prices;
   #print (CRAP Dumper(%prices));
 }
 #}}}
+sub calc_currency {
+  my @types = qw(oficial blue);
+  @types = map { $_ . '_euro' } @types if (shift(@_) eq 'euro');
 
+  my $output = '';
+  foreach my $type (@types) {
+    $output .= '[' . ucfirst($type) . '] $'  . sprintf("%.1f", $prices{$type}->{'value_buy' }) 
+                                    .' - $'  . sprintf("%.1f", $prices{$type}->{'value_sell'})
+                                    . ' :: ';
+  }
+  $output .= '[Solidario] $' . sprintf("%.1f", eval($prices{$types[0]}->{'value_sell'} * 1.3));
+  return $output;
+}
 #{{{ do_dolar
 sub do_dolar {
   my ($server, $chan, $text) = @_;
   my ($ask, $how_much) = $text =~ /^!(\w+)(\s+\d+(?:\.\d{1,2})?)?/;
 
-  get_price($bluelytics_url) if (time() - $last_fetch > $bufferme);
+  fetch_price($bluelytics_url) if (time() - $last_fetch > $bufferme);
 
   if ($ask =~ /^dol[oae]r$/) {
-    if (!$how_much) {
-      my $output = '[Oficial] $' . sprintf("%.1f", $prices{'oficial'}->{'value_buy'})
-                        . ' - $' . sprintf("%.1f", $prices{'oficial'}->{'value_sell'})
-                 . ' :: '
-                 . '[Blue] $'    . sprintf("%.1f", $prices{'blue'}->{'value_buy'})
-                        . ' - $' . sprintf("%.1f", $prices{'blue'}->{'value_sell'})
-                 . ' :: '
-                 . '[Solidario] $' . sprintf("%.1f", eval($prices{'oficial'}->{'value_sell'} * 1.3));
-
-      sayit($server, $chan, $output);
-    }
+    my $output = calc_currency('dollar');
+    #if (!$how_much) {
+    sayit($server, $chan, $output);
   } 
   elsif ($ask =~ /^euros?$/) {
-    my $euros = '[Euro Oficial] $' . sprintf("%.1f", $prices{'oficial_euro'}->{'value_buy'})
-                     . ' - $' . sprintf("%.1f", $prices{'oficial_euro'}->{'value_sell'})
-               . ' :: '
-               . '[Euro Blue] $'   . sprintf("%.1f", $prices{'blue_euro'}->{'value_buy'})
-                     . ' - $' . sprintf("%.1f", $prices{'blue_euro'}->{'value_sell'})
-               . ' :: '
-               . '[Euro Solidario] $' . sprintf("%.1f", eval($prices{'oficial_euro'}->{'value_sell'} * 1.3));
-
+    my $euros = calc_currency('euro');
     sayit($server, $chan, $euros);
   }
   return;
 }
 #}}}
-
 #{{{ signal and stuff
 sub sayit { my $s = shift; $s->command("MSG @_"); }
 signal_add("showme the money","do_dolar");
