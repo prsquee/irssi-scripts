c12ed8e486363af5dde130aaa575a93fc8806f7c
sQuEE <squee@fnord.com.ar>
2024-01-25 01:11:50 -0300

    manually convert json to mensual format

--- feriados.pl
+++ feriados.pl
@@ -22,28 +22,35 @@ my @meses = ('Enero', 'Febrero', 'Marzo', 'Abril','Mayo','Junio', 'Julio', 'Agos
 
 sub fetch_holidays {
   my ($server, $chan) = @_;
-  my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+  my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
   $year += 1900;
 
   my $ua  = LWP::UserAgent->new( timeout => 5 );
   $ua->agent(Irssi::settings_get_str('myUserAgent'));
 
-  my $raw = $ua->get($api_url . $year . '?formato=mensual')->content();
-  my $holidays = $json->utf8->decode($raw);
-  #print (CRAP Dumper($holidays));
+  my $raw = $ua->get($api_url . $year)->content();
+  my $holidays_unsorted = $json->utf8->decode($raw);
+  #print (CRAP Dumper($holidays_unsorted));
 
-  my $today = new_dt($year,$mon,$mday);
+  #format=mensual doesnt work anymore, convert it manually:
+  # @months is a 12 elemtns array, where each member is a hash table containing the holidays for that month:
+  # if a month doesnt have a holiday, it should be empty.
+
+  my @months = map { {} } (1..12);
+  foreach my $holiday (@{$holidays_unsorted}) {
+    $months[$holiday->{'mes'} - 1]->{$holiday->{'dia'}} = $holiday;
+  }
+
+  my $today = new_dt($year, $mon, $mday);
 
   # go to a month that has a holiday
-  while (not %{$$holidays[$mon]}) {
+  while (not %{$months[$mon]}) {
     $mon += 1;
   }
 
-  #print (CRAP Dumper($$holidays[$mon]));
-
   my $output = undef;
   while (not defined($output)) {
-    foreach my $day (sort {$a <=> $b} keys %{$$holidays[$mon]}) {
+    foreach my $day (sort {$a <=> $b} keys %{$months[$mon]}) {
       my $this_holiday = new_dt($year,$mon,$day);
       next if ($today > $this_holiday);
       next if ($this_holiday->day_of_week =~ /[67]/);
@@ -55,40 +62,31 @@ sub fetch_holidays {
         my $delta = $today->delta_days($this_holiday)->delta_days();
         $output = $delta == 1 ? 'MAÑANA ES FERIADO! ' : "Faltan $delta días para el $day de $meses[$mon]: ";
       }
-      #print (CRAP "mon: ${mon} ; day: ${day}; ref($$holidays[$mon]{$day}");
       # check ref type before accessing 'tipo'. There could be a rare two holidays on the same day.
-      if ($output and ref($$holidays[$mon]{$day}) eq 'ARRAY') {
-        foreach my $ref (@{$$holidays[$mon]{$day}}) {
-          $output .= $ref->{'motivo'} . '.';
-        }
-        sayit($server, $chan, $output);
-        return;
-      }
-      elsif ($output and $$holidays[$mon]{$day}->{'tipo'} eq 'puente') {
+      if ($output and $months[$mon]{$day}->{'tipo'} eq 'puente') {
         #puente could be very unlikely a day before
-        if ($$holidays[$mon]{$day - 1}->{'motivo'}) {
+        if ($months[$mon]{$day - 1}->{'motivo'}) {
           $day -= 1;
           $output .= " Feriado puente con el $day de $meses[$mon]. ";
         }
         #the most likey case is the next day
-        elsif ($$holidays[$mon]{$day + 1}->{'motivo'}) {
+        elsif ($months[$mon]{$day + 1}->{'motivo'}) {
           $day += 1;
           $output .= " Feriado puente con el $day de $meses[$mon]. ";
         }
         # an edge case, puente is on a friday, then go to monday
-        elsif ($this_holiday->day_name eq 'Friday' and $$holidays[$mon]{$day + 3}->{'motivo'}) {
+        elsif ($this_holiday->day_name eq 'Friday' and $months[$mon]{$day + 3}->{'motivo'}) {
           $day += 3;
           $output .= " Feriado puente con el $day de $meses[$mon]. ";
         }
       }
-      sayit($server, $chan, $output . "$$holidays[$mon]{$day}->{'motivo'}.");
+      sayit($server, $chan, $output . "$months[$mon]{$day}->{'motivo'}.");
       return;
     }
     $mon += 1 unless $output;
     if ($mon == 12) {
       sayit($server,$chan, 'no hay más feriados hasta el próximo año.');
       return;
-      # december.is $mon = 11
     }
   }
 }
