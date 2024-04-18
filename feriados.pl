a322940503c34de62440a57f0f9d4c44607f2c34
sQuEE <squee@fnord.com.ar>
2024-04-18 01:12:55 -0300

    fixed stuff

--- feriados.pl
+++ feriados.pl
@@ -1,5 +1,5 @@
 # feriados
-use Irssi qw(signal_add print settings_add_str settings_get_str);
+use Irssi qw(signal_add print settings_add_str settings_get_str get_irssi_dir);
 use strict;
 use warnings;
 use LWP::UserAgent;
@@ -7,6 +7,7 @@ use JSON;
 use Data::Dumper;
 use DateTime;
 use Scalar::Util;
+use Storable qw (store retrieve);
 
 use utf8;
 use Encode qw (encode decode);
@@ -17,6 +18,7 @@ signal_add("worknowork", "fetch_holidays");
 my $json = JSON->new();
 my $api_url = 'http://nolaborables.com.ar/api/v2/feriados/';
 my $last_fetch = 0;
+my $feriado_storable = get_irssi_dir() . '/scripts/datafiles/feriados.storable';
 
 my @meses = ('Enero', 'Febrero', 'Marzo', 'Abril','Mayo','Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
 
@@ -27,19 +29,27 @@ sub fetch_holidays {
 
   my $ua  = LWP::UserAgent->new( timeout => 5 );
   $ua->agent(Irssi::settings_get_str('myUserAgent'));
-
   my $raw = $ua->get($api_url . $year)->content();
-  my $holidays_unsorted = $json->utf8->decode($raw);
-  #print (CRAP Dumper($holidays_unsorted));
+
+  my $holidays_unsorted;
+  my @months;
+
+  $holidays_unsorted = $json->utf8->decode($raw);
+  if ($@) {
+    $holidays_unsorted = retrive ($feriado_storable);
+  }
+  else {
+    @months = map { {} } (1..12);
+    foreach my $holiday (@{$holidays_unsorted}) {
+      $months[$holiday->{'mes'} - 1]->{$holiday->{'dia'}} = $holiday;
+    }
+    store \@months, $feriado_storable;
+  }
 
   #format=mensual doesnt work anymore, convert it manually:
   # @months is a 12 elemtns array, where each member is a hash table containing the holidays for that month:
   # if a month doesnt have a holiday, it should be empty.
 
-  my @months = map { {} } (1..12);
-  foreach my $holiday (@{$holidays_unsorted}) {
-    $months[$holiday->{'mes'} - 1]->{$holiday->{'dia'}} = $holiday;
-  }
 
   my $today = new_dt($year, $mon, $mday);
 
@@ -62,23 +72,21 @@ sub fetch_holidays {
         my $delta = $today->delta_days($this_holiday)->delta_days();
         $output = $delta == 1 ? 'MAÑANA ES FERIADO! ' : "Faltan $delta días para el $day de $meses[$mon]: ";
       }
-      # check ref type before accessing 'tipo'. There could be a rare two holidays on the same day.
+
       if ($output and $months[$mon]{$day}->{'tipo'} eq 'puente') {
         #puente could be very unlikely a day before
         if ($months[$mon]{$day - 1}->{'motivo'}) {
           $day -= 1;
-          $output .= " Feriado puente con el $day de $meses[$mon]. ";
         }
         #the most likey case is the next day
         elsif ($months[$mon]{$day + 1}->{'motivo'}) {
           $day += 1;
-          $output .= " Feriado puente con el $day de $meses[$mon]. ";
         }
         # an edge case, puente is on a friday, then go to monday
         elsif ($this_holiday->day_name eq 'Friday' and $months[$mon]{$day + 3}->{'motivo'}) {
           $day += 3;
-          $output .= " Feriado puente con el $day de $meses[$mon]. ";
         }
+        $output .= " Feriado puente con el $day de $meses[$mon]. ";
       }
       sayit($server, $chan, $output . "$months[$mon]{$day}->{'motivo'}.");
       return;
@@ -100,4 +108,5 @@ sub new_dt {
     time_zone => 'UTC'
   );
 }
+
 sub sayit { my $s = shift; $s->command("MSG @_"); }
